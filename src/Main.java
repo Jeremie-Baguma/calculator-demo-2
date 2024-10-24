6c768410741aa90d75af5c3afb4b7d6d49aa8f48
Jeremie-Baguma <jeremiebaguma54@gmail.com>
2024-10-24 18:35:49 +0300

    Added add method

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Add: " + add(3,4));
+    }
+
+    public static int add(int x, int y) {
+        return  x +y ;
     }
 }
